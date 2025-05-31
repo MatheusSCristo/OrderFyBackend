e164bd53333613443212926680a77f0b550064fa
MatheusSCristo <cristo.matheus@outlook.com>
2025-05-31 14:13:36 -0300

    feat: add security configuration

--- src/main/java/com/orderfy/backend/controller/PingController.java
+++ src/main/java/com/orderfy/backend/controller/PingController.java
@@ -1,6 +1,7 @@
 package com.orderfy.backend.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ping")
 public class PingController {
 
+    @GetMapping("")
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok().body("Pong");
     }
